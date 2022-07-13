8170f99ef56fd5999e29f4900a095fff0370ccf9
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-07-13 14:12:03 +0300

    Added Foter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import styles from "./footer.module.scss";
+import Icons from "../Icons";
 
 const Footer = () => {
     return(
-        <footer>
-            <div className="container">
-                <p>Footer</p>
+        <footer className={styles.footer}>
+            <div className={`${styles.wrapper} container`}>
+                <a href="/">
+                    <Icons id="logo" />
+                </a>
             </div>
         </footer>
     )
