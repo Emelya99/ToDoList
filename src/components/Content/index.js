ed5f46079de7d626f4a362c3efa533e51629f145
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-07-13 14:12:22 +0300

    Added svg genegator

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,9 +1,14 @@
 import styles from "./content.module.scss";
+import Icons from "../Icons";
 
 const Content = () => {
     return(
         <main>
             <h2>Content</h2>
+            <Icons id="edit" />
+            <Icons id="circle" />
+            <Icons id="done" />
+            <Icons id="close" />
         </main>
     )
 }
